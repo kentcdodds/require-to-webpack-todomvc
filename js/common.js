fd5db3fe2aa1c7f7eee074ef8d7e4c86589fdcf6
Kent C. Dodds <kent@doddsfamily.us>
2015-12-15 01:29:28 -0800

    Turn tabs into spaces for my own sanity

--- js/common.js
+++ js/common.js
@@ -2,12 +2,12 @@
 'use strict';
 
 define([], function () {
-	return {
-		// Which filter are we using?
-		TodoFilter: '', // empty, active, completed
+  return {
+    // Which filter are we using?
+    TodoFilter: '', // empty, active, completed
 
-		// What is the enter key constant?
-		ENTER_KEY: 13,
-		ESCAPE_KEY: 27
-	};
+    // What is the enter key constant?
+    ENTER_KEY: 13,
+    ESCAPE_KEY: 27
+  };
 });
